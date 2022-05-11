865335298d23c870bd52c24e8fa457be71517af5
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-05-11 17:20:58 +0530

    [TOMESHWAR]ADD:-DELETE LAST ELEMENT

--- LinkedListPracticeProblem/Program.cs
+++ LinkedListPracticeProblem/Program.cs
@@ -2,45 +2,59 @@
 using System.Collections.Generic;
 using LinkedListPracticeProblem;
 
-Console.WriteLine("Welcome Linklist Practice Problem");
-Console.WriteLine("1. UC1-CrateLInkList\n2. UC2-Append LinkLis\n3. UC3-Insert Between\n4. UC4- DeleteFirstElement");
-Console.WriteLine("Take an option to execute");
-int Option = Convert.ToInt32(Console.ReadLine());
-switch (Option)
+Console.WriteLine("Welcome to Linkedlist Practice Problem");
+bool check = true;
+while (check)
 {
-    case 1:
-        LinkedList list = new LinkedList();
-        list.Add(56);
-        list.Add(30);
-        list.Add(70);
-        list.Display();
-        break;
+    Console.WriteLine("Welcome Linklist Practice Problem");
+    Console.WriteLine("1. UC1-CrateLInkList\n2. UC2-Append LinkLis\n3. UC3-Insert Between\n4. UC4- DeleteFirstElement\n5. UC5- DeleteLastElement");
+    Console.WriteLine("Take an option to execute");
+    int Option = Convert.ToInt32(Console.ReadLine());
+    switch (Option)
+    {
+        case 1:
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            break;
 
-    case 2:
-        UC2AppendLinkedList list1 = new UC2AppendLinkedList();
-        list1.Add(56);
-        list1.Append(30);
-        list1.Append(70);
-        break;
+        case 2:
+            UC2AppendLinkedList list1 = new UC2AppendLinkedList();
+            list1.Add(56);
+            list1.Append(30);
+            list1.Append(70);
+            break;
 
-    case 3:
-        UC3AddBetween list2 = new UC3AddBetween();
-        list2.Add(56);
-        list2.Add(70);
-        int a = list2.Search(56);
-        list2.InsertAtPerticularPosition(a + 1, 30);
-        list2.Display();
-        break;
+        case 3:
+            UC3AddBetween list2 = new UC3AddBetween();
+            list2.Add(56);
+            list2.Add(70);
+            int a = list2.Search(56);
+            list2.InsertAtPerticularPosition(a + 1, 30);
+            list2.Display();
+            break;
 
-    case 4:
-        UC4DeleteFirstElement list3 = new UC4DeleteFirstElement();
-        Console.WriteLine("Enter the position of the Element");
-        int index = Convert.ToInt32(Console.ReadLine());
-        list3.DeleteNodeAtPerticularPosition(index);
-        list3.Display();
-        break;
+        case 4:
+            UC4DeleteFirstElement list3 = new UC4DeleteFirstElement();
+            Console.WriteLine("Enter the position of the Element");
+            int index = Convert.ToInt32(Console.ReadLine());
+            list3.DeleteNodeAtPerticularPosition(index);
+            list3.Display();
+            break;
 
-    default:
-        Console.WriteLine("Enter proper Option to Execute");
-        break;
+        case 5:
+            UC5DeleteLastElement list4 = new UC5DeleteLastElement();
+            list4.DeleteLastIndex();
+            list4.Display();
+            break;
+
+        case 0:
+            check = false;
+            break;
+        default:
+            Console.WriteLine("Enter proper Option to Execute");
+            break;
+    }
 }
---
+++ LinkedListPracticeProblem/UC5DeleteLastElement.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListPracticeProblem;
+
+internal class UC5DeleteLastElement
+{
+    public Node head;
+    public void Add(int data)
+    {
+        Node node = new Node(data);
+        if (this.head == null)
+        {
+            this.head = node;
+        }
+        else
+        {
+            Node temp = head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = node;
+            Console.WriteLine(node.data + " " + "inserted into Linked List ");
+        }
+    }
+    public void Append(int data)
+    {
+        Node node = new Node(data);
+        Node temp = head;
+        while (temp.next != null)
+        {
+            temp = temp.next;
+        }
+        temp.next = node;
+        Console.WriteLine(node.data + " " + "Appended into Linked List ");
+        Display();
+
+    }
+    public void Display()
+    {
+        Node temp = head;
+        if (temp == null)
+        {
+            Console.WriteLine("There is no element int he linked List");
+        }
+        while (temp != null)
+        {
+            Console.Write(temp.data + "  ");
+            temp = temp.next;
+        }
+        Console.WriteLine(" ");
+    }
+    public int Search(int addedData)
+    {
+        Node node = this.head;
+        int count = 0;
+        while (node != null)
+        {
+            if (node.data == addedData)
+            {
+                return count;
+            }
+            node = node.next;
+            count++;
+        }
+        return count;
+    }
+    public Node InsertAtPerticularPosition(int position, int data)
+    {
+        var newNode = new Node(data);
+        if (this.head == null)
+        {
+            return newNode;
+        }
+        if (position == 0)
+        {
+            newNode.next = head;
+            head = newNode;
+            return head;
+        }
+        Node prev = null;
+        Node current = this.head;
+        int count = 0;
+        while (current != null && count < position)
+        {
+
+
+            prev = current;
+            current = current.next;
+            count++;
+        }
+        newNode.next = prev.next;
+        prev.next = newNode;
+        return this.head;
+    }
+    public void DeleteNodeAtPerticularPosition(int position)
+    {
+        if (this.head == null)
+        {
+            Console.WriteLine("Linked List is Empty");
+        }
+        Node temp = this.head;
+        if (position == 0)
+        {
+            this.head = temp.next;
+        }
+        for (int i = 0; temp != null && i < position - 1; i++)
+        {
+            temp = temp.next;
+        }
+        if (temp == null || temp.next == null) { }
+        {
+            return;
+        }
+        Node next = temp.next.next;
+        temp.next = null; ;
+    }
+    public Node DeleteLastIndex()
+    {
+        if (head == null)
+        {
+            return null;
+        }
+        Node newNode = head;
+        while (newNode.next.next != null)
+        {
+            newNode = newNode.next;
+        }
+        newNode.next = null;
+        return head;
+    }
+
+}
